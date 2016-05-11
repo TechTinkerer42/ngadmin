b976465a55aa3ab7b21fea348ac6bbad23c253c4
fripp0971 <frippware@gmail.com>
2016-05-11 15:16:30 -0400

    removed js

--- app/CustomerAccount/service/customer-account-model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var CustomerAccount = (function () {
-    function CustomerAccount() {
-        this.newAccount = false;
-    }
-    return CustomerAccount;
-}());
-exports.CustomerAccount = CustomerAccount;
-//# sourceMappingURL=customer-account-model.js.map
